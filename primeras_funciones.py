dbb097530480610032ed3886d5c896a5f03757ef
Pedro Tambriz <pedro.tambriz.ag@gmail.com>
2022-01-17 15:57:04 -0600

    Change name from prueba.py to primeras_funciones.py and added additional information to primeras_funciones.py

---
+++ primeras_funciones.py
@@ -0,0 +1,14 @@
+# Declaracion de la funciona
+def mensaje():
+
+    # El cuerdo de la fucnion
+    print("Estamos aprendiendo Python")
+    print("Estamos aprendiendo instrucciones basicas")
+    print("Poco a poco iremos avanzando")
+    print()
+    
+# La llamada a la funcion
+mensaje()
+mensaje()
+mensaje()
+mensaje()
